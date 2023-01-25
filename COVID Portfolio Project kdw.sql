6fc38254964ecbfb4e22734cc5cad570a1ed87f7
KDWNKB <123221553+KDWNKB@users.noreply.github.com>
2023-01-24 19:49:20 -0600

    Update COVID Portfolio Project kdw.sql

--- COVID Portfolio Project kdw.sql
+++ COVID Portfolio Project kdw.sql
@@ -8,7 +8,7 @@ order by 3,4
 --From [Portfolio Project ]..CovidVaccinations
 --order by 3,4
 
---Select Data that we are going to be using
+--Selecting the Data that I will be using
 
 Select Location, date, total_cases, new_cases, total_deaths, population
 From [Portfolio Project ]..CovidDeaths
@@ -16,7 +16,7 @@ order by 1,2
 
 
 --Looking at Total Cases vs Total Deaths 
---Shows the likelihod of dying if you contract COVID in your country
+--Shows the likelihod of dying if you contract COVID in the US. This can be done for every country around the world
 Select Location, date, total_cases, total_deaths, (total_deaths/total_cases)*100 as DeathPercentage
 From [Portfolio Project ]..CovidDeaths
 Where location like '%states%'
@@ -25,14 +25,14 @@ order by 1,2
 
 
 -- Looking at the Total Cases vs Population
---Shows what percentage of population git COVID
+--Shows what percentage of population contracted COVID
 
 Select Location, date, population, total_cases, (total_cases/population)*100 as PercentofPopulationInfected
 From [Portfolio Project ]..CovidDeaths
 --Where location like '%states%'
 order by 1,2
 
---Looking at countries with the Highest Infection Rate Comparedto Population 
+--Looking at countries with the Highest Infection Rate Compared to Population 
 
 Select Location, population, MAX(total_cases)as HighestInfectionCount, Max((total_cases/population))*100 as PercentofPopulationInfected
 From [Portfolio Project ]..CovidDeaths
@@ -51,7 +51,7 @@ Group by location, population
 order by TotalDeathCount desc
 
 
---LETS BREAK THINGS DOWN BY CONTINENT
+--Breaking things down by Continent 
 
 --Showing continents with the highest death count per population
 
@@ -87,7 +87,7 @@ Where dea.continent is not null
 order by 2,3
 
 
---USE CTE 
+--USING CTE 
 
 With PopvsVac (Continent, Location, Date, Population, New_Vaccinations, RollingPeopleVaccinated)
 as
